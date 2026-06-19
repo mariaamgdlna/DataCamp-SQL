7bec16bc1bf0e32c1f8fc53f1f6e6a2ca0d49a2a
Maria Magdalena Clintina Nababan <mariaamgdlna@gmail.com>
2026-06-19 14:55:10 +0700

    Create Select-Subquery1.sql

---
+++ Subqueries/Select-Subquery1.sql
@@ -0,0 +1,22 @@
+SELECT
+    l.name AS league,
+    ROUND(AVG(m.home_goal + m.away_goal), 2) AS avg_goals,
+
+    ROUND(
+        AVG(m.home_goal + m.away_goal) -
+        (
+            SELECT AVG(home_goal + away_goal)
+            FROM match
+            WHERE season = '2013/2014'
+        ),
+        2
+    ) AS diff
+
+FROM league AS l
+LEFT JOIN match AS m
+ON l.country_id = m.country_id
+
+-- Only include 2013/2014 results
+WHERE season = '2013/2014'
+
+GROUP BY l.name;
