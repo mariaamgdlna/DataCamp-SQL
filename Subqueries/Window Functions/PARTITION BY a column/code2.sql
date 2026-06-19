e13408e618a4e63b8a25939a5b9b31b49f5e80a1
Maria Magdalena Clintina Nababan <mariaamgdlna@gmail.com>
2026-06-19 17:41:45 +0700

    Create code2.sql

---
+++ Subqueries/Window Functions/PARTITION BY a column/code2.sql
@@ -0,0 +1,22 @@
+SELECT 
+    date,
+    season,
+    home_goal,
+    away_goal,
+    CASE
+        WHEN hometeam_id = 8673 THEN 'home'
+        ELSE 'away'
+    END AS warsaw_location,
+    AVG(home_goal) OVER(
+        PARTITION BY season,
+        EXTRACT(MONTH FROM date)
+    ) AS season_mo_home,
+    AVG(away_goal) OVER(
+        PARTITION BY season,
+        EXTRACT(MONTH FROM date)
+    ) AS season_mo_away
+FROM match
+WHERE
+    hometeam_id = 8673
+    OR awayteam_id = 8673
+ORDER BY (home_goal + away_goal) DESC;
