67ca690d1763091caf50032639af39db9633c406
Maria Magdalena Clintina Nababan <mariaamgdlna@gmail.com>
2026-06-19 16:35:27 +0700

    Create code.sql

---
+++ Subqueries/Window Functions/PARTITION BY a column/code.sql
@@ -0,0 +1,17 @@
+SELECT
+    date,
+    season,
+    home_goal,
+    away_goal,
+    CASE
+        WHEN hometeam_id = 8673 THEN 'home'
+        ELSE 'away'
+    END AS warsaw_location,
+
+    AVG(home_goal) OVER(PARTITION BY season) AS season_homeavg,
+    AVG(away_goal) OVER(PARTITION BY season) AS season_awayavg
+FROM match
+WHERE
+    hometeam_id = 8673
+    OR awayteam_id = 8673
+ORDER BY (home_goal + away_goal) DESC;
